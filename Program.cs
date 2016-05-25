95139f8f00f1a4c8ce5a452590b584558b5cd873
HBPro360 <jphamm360@gmail.com>
2016-05-24 21:28:43 -0500

    WorkingWithQueues

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithQueues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            Queue<int> nums = new Queue<int>();
+            for(int i = 0; i < 10; i++)
+            {
+                int num = rnd.Next(0, 1000);
+                nums.Enqueue(num);
+                Console.WriteLine(i.ToString() + " " + num);
+            }
+            Console.WriteLine();
+            for(int i = 0; i < 10; i++)
+            {
+                int num = nums.Dequeue();
+                Console.WriteLine(i.ToString() + " " + num);
+            }
+            Console.WriteLine("Press Enter to continue.....");
+            Console.ReadLine();
+
+        }
+    }
+}
